6dd0c700c1c2d99f48490efde04199c62ddeb1d6
BryanEnriquez15 <159509904+BryanEnriquez15@users.noreply.github.com>
2024-02-08 23:36:52 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,3 @@
-print('In Pima East campus there is a student called (Bryan).
+print('In Pima East campus there is a student called Bryan')
+Print('Bryan entered his class called cis129')
+print('Bryan left his class gaining more knowledge in coding')
